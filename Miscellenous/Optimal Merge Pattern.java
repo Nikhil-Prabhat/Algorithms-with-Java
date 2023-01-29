f38f019507ed06172f10d94f44576fd0ac9904ec
Nikhil-Prabhat <62531542+Nikhil-Prabhat@users.noreply.github.com>
2023-01-29 12:56:33 +0530

    Optimal Merge Pattern

---
+++ Miscellenous/Optimal Merge Pattern.java
@@ -0,0 +1,42 @@
+package Miscellenous;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/*
+* Time Complexity : O(nlogn)
+* 
+* 2   3     4
+* \  /    /
+*   5   /
+*   \  /
+*    9
+* */
+
+public class OptimalMergePattern {
+
+    public static void main(String[] args) {
+        int files[] = new int[] { 2, 3, 4, 5, 6, 7 };
+        System.out.println(OptimalMergePattern.minComputation(files));
+    }
+
+    private static int minComputation(int[] files) {
+
+        // Create a min-heap and add sizes to the priority queue
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        Arrays.stream(files).forEach(
+                a -> minHeap.add(a)
+        );
+
+        int totalCount = 0;
+
+        // Pop minimum two elements and add the cost obtaine to the heap again
+        while (minHeap.size() > 1) {
+            int tempCost = minHeap.poll() + minHeap.poll();
+            totalCount += tempCost;
+            minHeap.add(tempCost);
+        }
+
+        return totalCount;
+    }
+}
