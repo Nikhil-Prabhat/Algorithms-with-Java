71413a7f2f904296051e2ccf91cd174ca422e5a0
Nikhil-Prabhat <62531542+Nikhil-Prabhat@users.noreply.github.com>
2023-01-29 11:54:52 +0530

    Dynamic Programming Knapsack Update

--- Miscellenous/KnapsackProblem.java
+++ Miscellenous/KnapsackProblem.java
@@ -6,6 +6,28 @@ Knapsack Problem is of two types :
 1. 0/1 Knapsack Problem         : Splitting of items is not allowed
 2. Fractional Knapsack Problem  : Splitting of items is allowed
 
+Time Complexity of Greddy Approach : O(n)
+Time Complexity of DP Approach     : O(n^2)
+
+## Table Creation Example
+
+int W[] = new int[]{12, 2, 1, 1, 4};
+int V[] = new int[]{4, 2, 1, 2, 10};
+
+int M = 15;
+int n = V.length;
+
+0 0 0 3 3 3 3 3 3 3 3 3
+0 0 0 3 4 4 4 7 7 7 7 7
+0 0 0 3 4 4 4 7 7 8 8 8
+0 0 0 3 4 4 4 7 7 10 10 10
+0 0 0 3 4 4 4 7 8 10 10 11
+Max Value:	11
+Selected Packs:
+	Package 5 with W = 4 and Value = 4
+	Package 2 with W = 4 and Value = 4
+	Package 1 with W = 3 and Value = 3
+
  */
 
 
@@ -16,6 +38,8 @@ public class KnapsackProblem {
         int[] profits = new int[]{10, 5, 15, 7, 6, 18, 3};
         int[] weights = new int[]{2, 3, 5, 7, 1, 4, 1};
         KnapsackProblem.findKnapsack(profits,weights,15);
+        
+        findKnapsackWithDP();
 
     }
 
@@ -62,5 +86,51 @@ public class KnapsackProblem {
             }
         }
         return index;
+    }
+    
+    // This is a Dynamic Programming Approach
+    private static void findKnapsackWithDP() {
+        // Always include 0 as the first row and first column
+        int[] profits = new int[]{0, 1, 2, 5, 6};
+        int[] weights = new int[]{0, 2, 3, 4, 5};
+        int bagCapacity = 8;
+        int objCount = 4;
+        int maximumWeight = 0;
+
+        // The row should be 1 more than the objCount and the column should be 1 more than the bagCapacity
+        int[][] computeMatrix = new int[5][9];
+
+        for (int i = 0; i <= objCount; i++) {
+            for (int w = 0; w <= bagCapacity; w++) {
+                // For first row and first column
+                if (i == 0 || w == 0) {
+                    computeMatrix[i][w] = 0;
+                } else if (weights[i] <= w) {
+                    computeMatrix[i][w] = Math.max(profits[i] + computeMatrix[i - 1][w - weights[i]], computeMatrix[i - 1][w]);
+                } else {
+                    // Copy the previous column value when the column weight is not greater than the actual weight
+                    computeMatrix[i][w] = computeMatrix[i - 1][w];
+                }
+            }
+        }
+
+        // Maximum weight to be included
+        maximumWeight = computeMatrix[objCount][bagCapacity];
+        System.out.println("Maximum weight to be included in the bag : " + maximumWeight);
+
+        // Find the objects to be included
+        int i = objCount;
+        int j = bagCapacity;
+
+        while (i > 0 && j > 0) {
+            if (computeMatrix[i][j] == computeMatrix[i - 1][j]) {
+                System.out.println(i + " 0");
+                i--;
+            } else {
+                System.out.println(i + " 1");
+                j = j - weights[i];
+                i--;
+            }
+        }
     }
 }
